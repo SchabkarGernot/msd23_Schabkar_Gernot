9319158811e830a3ce79d77137493522179ab37d
Gernot Schabkar <gernot.schabkar@edu.fh-joanneum.at>
2024-06-12 22:56:23 +0200

    Faculty 0 implementet, tests fail.

--- src/main/java/msd23_Schabkar_Gernot/src/main/java/at/fhj/msd/Main.java
+++ src/main/java/msd23_Schabkar_Gernot/src/main/java/at/fhj/msd/Main.java
@@ -23,12 +23,20 @@ public class Main {
     public static double multiply(double number1, double number2) {
         return number1 * number2;
     }
+
+    public static int factorial(int n) {
+        return 0;
+        //if (n<=1) return 1;
+        //return n*factorial(n-1);
+    }
     Main() {
         System.out.println("5-4:" + minus(5,4));
         System.out.println("5+4:" + add(5,4));
         System.out.println("5/4:" + divide(5,4));
         System.out.println("5*4:" + multiply(5,4));
+        System.out.println( "Factor of 5:" + factorial(5));
     }
+
 
     public static void main(String[] args) {
         Main calc = new Main();
